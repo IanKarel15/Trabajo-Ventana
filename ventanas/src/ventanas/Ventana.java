4568d4dad24e145867d3c373a128d1ef1cd3868f
IanKarel15 <iankareld_23@alu.uabcs.mx>
2025-02-13 17:10:12 -0700

    unidad 2 ejercicio 7

--- ventanas/src/ventanas/Ventana.java
+++ ventanas/src/ventanas/Ventana.java
@@ -1,22 +1,51 @@
 package ventanas;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class Ventana extends JFrame {
 
-	public Ventana() {
-		
-		setSize(500, 500);
+    private JPanel panel;
+    private JLabel etiqueta;
+
+    public Ventana() {
+        configurarVentana();
+        inicializarComponentes();
+    }
+
+    private void configurarVentana() {
+        setSize(500, 500);
         setTitle("Ventana");
         setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	public static void main (String[] args) {
-		Ventana v1 = new Ventana();
-		
-		v1.setVisible(true);
-		
-	}
+    }
+
+    private void inicializarComponentes() {
+        panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        panel.setLayout(null); 
+        this.getContentPane().add(panel);
+
+        etiqueta = new JLabel("1er Etiqueta");
+        etiqueta.setBounds(140, 50, 200, 50);
+        etiqueta.setForeground(Color.BLACK);
+        etiqueta.setOpaque(true);
+        etiqueta.setBackground(Color.WHITE);
+        etiqueta.setFont(new Font("Arial", Font.PLAIN, 35));
+
+        panel.add(etiqueta);
+    }
+
+    public static void main(String[] args) {
+        
+    	Ventana v1 = new Ventana();
+        v1.setVisible(true);
+        
+    }
 }
